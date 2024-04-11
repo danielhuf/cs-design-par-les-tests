d1da261c5e69d489f2f330e96dd716cd64761872
Daniel Huf <daniel.stulberg-huf@student-cs.fr>
2024-04-11 15:26:42 +0200

    createGroup green test 2

--- commoncents/src/entities/Group.ts
+++ commoncents/src/entities/Group.ts
@@ -2,7 +2,7 @@ export class Group {
     name: string;
     members: string[];
 
-    constructor(name: string, members: string[]) {
+    constructor(name: string, members: string[] = []) {
         this.name = name;
         this.members = members;
     }
