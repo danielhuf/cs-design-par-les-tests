4e0be365d9401b64cb576febc463966cb00450fd
largoui <largou.imane.mpsi@gmail.com>
2024-04-15 17:12:46 +0200

    Add total balance

--- commoncents/src/entities/Group.ts
+++ commoncents/src/entities/Group.ts
@@ -5,12 +5,14 @@ export class Group {
     name: string;
     members: Member[];
     expenses: Expense[];
+    total_balance: number;
 
     constructor(id: string, name: string, members: Member[] = []) {
         this.id = id;
         this.name = name;
         this.members = members;
         this.expenses = [];
+        this.total_balance = 0;
     }
 
     addMember(member: Member): void {
@@ -19,5 +21,10 @@ export class Group {
 
     addExpense(expense: Expense): void {
         this.expenses.push(expense);
+        this.calculateTotalBalance();
+    }
+
+    calculateTotalBalance(): void {
+        this.total_balance = this.expenses.reduce((acc, expense) => acc + expense.amount, 0);
     }
 }
