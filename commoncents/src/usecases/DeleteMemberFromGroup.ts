4c1619e638958889d4476f456e0f38c566c831b5
Daniel Huf <daniel.stulberg-huf@student-cs.fr>
2024-04-15 08:53:09 +0200

    [GREEN TEST] delete member from group

--- commoncents/src/usecases/DeleteMemberFromGroup.ts
+++ commoncents/src/usecases/DeleteMemberFromGroup.ts
@@ -1,5 +1,21 @@
 import { IGroupRepository } from "../interfaces/repositories/IGroupRepository";
 
 export class DeleteMemberFromGroup {
+    private repository: IGroupRepository;
 
+    constructor(repository: IGroupRepository) {
+        this.repository = repository;
+    }
+
+    execute(groupId: string, memberName: string): void {
+        const group = this.repository.findGroup(groupId);
+        if (!group) {
+            throw new Error("Group not found");
+        }
+        const memberIndex = group.members.findIndex(member => member.name === memberName);
+        if (memberIndex === -1) {
+            throw new Error("Member not found");
+        }
+        group.members.splice(memberIndex, 1);
+    }
 }
