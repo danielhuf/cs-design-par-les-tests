c3a74b9433b104636872c3a57151857f145e8abc
Tomas Gonzalez <tgonzalezv@uc.cl>
2024-04-16 21:21:32 +0200

    [GREEN TEST] groupController create group

--- commoncents/src/frameworks/web/controllers/GroupController.ts
+++ commoncents/src/frameworks/web/controllers/GroupController.ts
@@ -1,4 +1,22 @@
 import { IGroupController } from "../../../interfaces/controllers/IGroupController";
+import { CreateGroup } from "../../../usecases/CreateGroup";
+import { Group } from "../../../domain/entities/Group";
+import { Member } from "../../../domain/entities/Member";
 
 export class GroupController implements IGroupController {
+    private createGroupUseCase: CreateGroup;
+
+    constructor(createGroupUseCase: CreateGroup) {
+        this.createGroupUseCase = createGroupUseCase;
+    }
+
+    public async createGroup(groupName: string, memberNames: string[]): Promise<Group> {
+        const members = await this.createMembers(memberNames);
+        return this.createGroupUseCase.execute(groupName, members);
+    }
+
+    private async createMembers(memberNames: string[]): Promise<Member[]> {
+        return memberNames.map(name => new Member(name));
+    }
+
 }
--- commoncents/src/interfaces/controllers/IGroupController.ts
+++ commoncents/src/interfaces/controllers/IGroupController.ts
@@ -1,2 +1,5 @@
+import { Group } from "../../domain/entities/Group";
+
 export interface IGroupController {
+  createGroup(groupName: string, memberNames: string[]): Promise<Group>;
 }
