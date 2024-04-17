12d404f0dfc4bdc0415a49873a5b4f6a2f759139
Tomas Gonzalez <tgonzalezv@uc.cl>
2024-04-17 02:35:07 +0200

    style: increase tab size

--- commoncents/tests/controllerTests/createGroupController.test.ts
+++ commoncents/tests/controllerTests/createGroupController.test.ts
@@ -8,136 +8,136 @@ import { Member } from "../../src/domain/entities/Member";
 import { DeleteGroup } from "../../src/usecases/DeleteGroup";
 
 describe("GroupController", () => {
-  let createGroupUseCase: CreateGroup;
-  let groupController: GroupController;
-  let mockReq: Request;
-  let mockRes: Response;
-  let clearMockRes: () => void;
+    let createGroupUseCase: CreateGroup;
+    let groupController: GroupController;
+    let mockReq: Request;
+    let mockRes: Response;
+    let clearMockRes: () => void;
 
-  beforeEach(() => {
-    const mockGroupRepository: jest.Mocked<IGroupRepository> = {
-      addGroup: jest.fn(),
-      deleteGroup: jest.fn().mockReturnValue(true),
-      findGroup: jest.fn().mockReturnValue(undefined),
-    };
+    beforeEach(() => {
+        const mockGroupRepository: jest.Mocked<IGroupRepository> = {
+            addGroup: jest.fn(),
+            deleteGroup: jest.fn().mockReturnValue(true),
+            findGroup: jest.fn().mockReturnValue(undefined),
+        };
 
-    createGroupUseCase = new CreateGroup(mockGroupRepository);
-    groupController = new GroupController(createGroupUseCase, new DeleteGroup(mockGroupRepository));
-    mockReq = getMockReq();
-    let { res, mockClear } = getMockRes();
-    mockRes = res;
-    clearMockRes = mockClear;
-  });
+        createGroupUseCase = new CreateGroup(mockGroupRepository);
+        groupController = new GroupController(createGroupUseCase, new DeleteGroup(mockGroupRepository));
+        mockReq = getMockReq();
+        let { res, mockClear } = getMockRes();
+        mockRes = res;
+        clearMockRes = mockClear;
+    });
 
-  afterEach(() => {
-    clearMockRes();
-  });
+    afterEach(() => {
+        clearMockRes();
+    });
 
-  it("createGroup should successfully create a group with members and return the appropriate response", async () => {
-    // Arrange
-    mockReq.body = { name: "Adventure Club", members: ["Alice", "Bob"] };
-    
-    const expectedResponse = {
-      id: "1",
-      name: "Adventure Club",
-      members: [{ name: "Alice" }, { name: "Bob" }]
-    };
+    it("createGroup should successfully create a group with members and return the appropriate response", async () => {
+        // Arrange
+        mockReq.body = { name: "Adventure Club", members: ["Alice", "Bob"] };
 
-    jest.spyOn(createGroupUseCase, "execute").mockReturnValue(
-      new Group("1", "Adventure Club", [
-        new Member("Alice"),
-        new Member("Bob"),
-      ])
-    );
+        const expectedResponse = {
+            id: "1",
+            name: "Adventure Club",
+            members: [{ name: "Alice" }, { name: "Bob" }]
+        };
 
-    // Act
-    await groupController.createGroup(mockReq, mockRes);
+        jest.spyOn(createGroupUseCase, "execute").mockReturnValue(
+            new Group("1", "Adventure Club", [
+            new Member("Alice"),
+            new Member("Bob"),
+            ])
+        );
 
-    // Assert
-    expect(mockRes.status).toHaveBeenCalledWith(201);
-    expect(mockRes.json).toHaveBeenCalledWith(expectedResponse);
-  });
+        // Act
+        await groupController.createGroup(mockReq, mockRes);
 
-  it("createGroup should return 400 if the request body is empty", async () => {
-    // Arrange
-    mockReq.body = {};
+        // Assert
+        expect(mockRes.status).toHaveBeenCalledWith(201);
+        expect(mockRes.json).toHaveBeenCalledWith(expectedResponse);
+    });
 
-    // Act
-    await groupController.createGroup(mockReq, mockRes);
+    it("createGroup should return 400 if the request body is empty", async () => {
+        // Arrange
+        mockReq.body = {};
 
-    // Assert
-    expect(mockRes.status).toHaveBeenCalledWith(400);
-    expect(mockRes.json).toHaveBeenCalledWith({ message: "Invalid request body" });
-  });
+        // Act
+        await groupController.createGroup(mockReq, mockRes);
 
-  it("createGroup should return 400 if the request body is missing name", async () => {
-    // Arrange
-    mockReq.body = { members: ["Alice", "Bob"] };
+        // Assert
+        expect(mockRes.status).toHaveBeenCalledWith(400);
+        expect(mockRes.json).toHaveBeenCalledWith({ message: "Invalid request body" });
+    });
 
-    // Act
-    await groupController.createGroup(mockReq, mockRes);
+    it("createGroup should return 400 if the request body is missing name", async () => {
+        // Arrange
+        mockReq.body = { members: ["Alice", "Bob"] };
 
-    // Assert
-    expect(mockRes.status).toHaveBeenCalledWith(400);
-    expect(mockRes.json).toHaveBeenCalledWith({ message: "Invalid request body" });
-  });
+        // Act
+        await groupController.createGroup(mockReq, mockRes);
 
-  it("createGroup should return 400 if the name is empty", async () => {
-    // Arrange
-    mockReq.body = { name: "", members: ["Alice", "Bob"] };
+        // Assert
+        expect(mockRes.status).toHaveBeenCalledWith(400);
+        expect(mockRes.json).toHaveBeenCalledWith({ message: "Invalid request body" });
+    });
 
-    // Act
-    await groupController.createGroup(mockReq, mockRes);
+    it("createGroup should return 400 if the name is empty", async () => {
+        // Arrange
+        mockReq.body = { name: "", members: ["Alice", "Bob"] };
 
-    // Assert
-    expect(mockRes.status).toHaveBeenCalledWith(400);
-    expect(mockRes.json).toHaveBeenCalledWith({ message: "Invalid request body" });
-  });
+        // Act
+        await groupController.createGroup(mockReq, mockRes);
 
-  it("createGroup should successfully create a group with no members and return the appropriate response", async () => {
-    // Arrange
-    mockReq.body = { name: "Adventure Club" };
-    
-    const expectedResponse = {
-      id: "1",
-      name: "Adventure Club",
-      members: []
-    };
+        // Assert
+        expect(mockRes.status).toHaveBeenCalledWith(400);
+        expect(mockRes.json).toHaveBeenCalledWith({ message: "Invalid request body" });
+    });
 
-    jest.spyOn(createGroupUseCase, "execute").mockReturnValue(
-      new Group("1", "Adventure Club", [])
-    );
+    it("createGroup should successfully create a group with no members and return the appropriate response", async () => {
+        // Arrange
+        mockReq.body = { name: "Adventure Club" };
+        
+        const expectedResponse = {
+        id: "1",
+        name: "Adventure Club",
+        members: []
+        };
 
-    // Act
-    await groupController.createGroup(mockReq, mockRes);
+        jest.spyOn(createGroupUseCase, "execute").mockReturnValue(
+            new Group("1", "Adventure Club", [])
+        );
 
-    // Assert
-    expect(mockRes.status).toHaveBeenCalledWith(201);
-    expect(mockRes.json).toHaveBeenCalledWith(expectedResponse);
-  });
+        // Act
+        await groupController.createGroup(mockReq, mockRes);
 
-  it("createGroup should return 400 if members is not an array", async () => {
-    // Arrange
-    mockReq.body = { name: "Adventure Club", members: "Alice" };
+        // Assert
+        expect(mockRes.status).toHaveBeenCalledWith(201);
+        expect(mockRes.json).toHaveBeenCalledWith(expectedResponse);
+    });
 
-    // Act
-    await groupController.createGroup(mockReq, mockRes);
+    it("createGroup should return 400 if members is not an array", async () => {
+        // Arrange
+        mockReq.body = { name: "Adventure Club", members: "Alice" };
 
-    // Assert
-    expect(mockRes.status).toHaveBeenCalledWith(400);
-    expect(mockRes.json).toHaveBeenCalledWith({ message: "Invalid request body" });
-  });
+        // Act
+        await groupController.createGroup(mockReq, mockRes);
 
-  it("createGroup should return 400 if members is not an array of strings", async () => {
-    // Arrange
-    mockReq.body = { name: "Adventure Club", members: ["Alice",
-      { name: "Bob" }] };
-    
-    // Act
-    await groupController.createGroup(mockReq, mockRes);
+        // Assert
+        expect(mockRes.status).toHaveBeenCalledWith(400);
+        expect(mockRes.json).toHaveBeenCalledWith({ message: "Invalid request body" });
+    });
 
-    // Assert
-    expect(mockRes.status).toHaveBeenCalledWith(400);
-    expect(mockRes.json).toHaveBeenCalledWith({ message: "Invalid request body" });
-  });
+    it("createGroup should return 400 if members is not an array of strings", async () => {
+        // Arrange
+        mockReq.body = { name: "Adventure Club", members: ["Alice",
+        { name: "Bob" }] };
+        
+        // Act
+        await groupController.createGroup(mockReq, mockRes);
+
+        // Assert
+        expect(mockRes.status).toHaveBeenCalledWith(400);
+        expect(mockRes.json).toHaveBeenCalledWith({ message: "Invalid request body" });
+    });
 });
