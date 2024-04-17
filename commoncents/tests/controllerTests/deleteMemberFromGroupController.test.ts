fc4d694299472cd08ecd953708f07ddc88c91504
Tomas Gonzalez <tgonzalezv@uc.cl>
2024-04-17 16:48:12 +0200

    red test: refactor deleteMemberFromGroup controller success

--- commoncents/tests/controllerTests/deleteMemberFromGroupController.test.ts
+++ commoncents/tests/controllerTests/deleteMemberFromGroupController.test.ts
@@ -5,6 +5,7 @@ import { Group } from "../../src/domain/entities/Group";
 import { IGroupRepository } from "../../src/interfaces/repositories/IGroupRepository";
 import { AddMemberToGroup } from "../../src/usecases/AddMemberToGroup";
 import { DeleteMemberFromGroup } from "../../src/usecases/DeleteMemberFromGroup";
+import { Member } from "../../src/domain/entities/Member";
 
 describe("MemberController", () => {
   let mockGroupRepository: jest.Mocked<IGroupRepository>;
@@ -26,14 +27,13 @@ describe("MemberController", () => {
   });
 
   it("should successfully delete a member from a group and return the appropriate response", async () => {
-      const group = new Group("123", "Test Group", []);
+      
       const memberName = "Alice";
-      req.params.memberName = memberName;
+      const group = new Group("123", "Test Group", [new Member(memberName)]);
+      req.params.member = memberName;
       mockGroupRepository.findGroup.mockReturnValueOnce(group);
 
       await memberController.deleteMemberFromGroup(req, res);
-
-      expect(deleteMemberFromGroupUseCase.execute).toHaveBeenCalledWith("123", "Alice");
       expect(res.status).toHaveBeenCalledWith(200);
       expect(res.json).toHaveBeenCalledWith({ message: `Successfully deleted ${memberName} from group` });
   });
