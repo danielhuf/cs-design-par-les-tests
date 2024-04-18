92e64c58740e2d9ea6b0345e1f97d8ea17bdd522
Tomas Gonzalez <tgonzalezv@uc.cl>
2024-04-18 20:56:22 +0200

    [GREEN TEST] memberAddsExpenseToGroupController 200

--- commoncents/src/frameworks/web/controllers/ExpenseController.ts
+++ commoncents/src/frameworks/web/controllers/ExpenseController.ts
@@ -1,2 +1,16 @@
+import { Request, Response } from "express";
+import { MemberAddsExpenseToGroup } from "../../../usecases/MemberAddsExpenseToGroup";
 export class ExpenseController {
+  private memberAddsExpenseToGroupUseCase: MemberAddsExpenseToGroup;
+
+  constructor(memberAddsExpenseToGroupUseCase: MemberAddsExpenseToGroup) {
+    this.memberAddsExpenseToGroupUseCase = memberAddsExpenseToGroupUseCase;
+  }
+
+  public async addExpenseToGroup(req: Request, res: Response): Promise<void> {
+    const { id } = req.params;
+    const { title, amount, payerName, date, isPercentual, split } = req.body;
+    this.memberAddsExpenseToGroupUseCase.execute(id, title, amount, payerName, date, isPercentual, split);
+    res.status(200).json({ success: true });
+  }
 }
