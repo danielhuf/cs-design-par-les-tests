02a10c43bb71e77882e8d7927475a802b6aac8e2
Tomas Gonzalez <tgonzalezv@uc.cl>
2024-04-18 18:15:16 +0200

    [REFACTORING] remove Member controller from API test

--- commoncents/tests/webTests/apiServer.test.ts
+++ commoncents/tests/webTests/apiServer.test.ts
@@ -7,23 +7,17 @@ import { RoutePaths } from '../../src/frameworks/web/routes/routeConfig';
 describe('API Server Tests', () => {
   let apiServer: ApiServer;
   let mockGroupController: jest.Mocked<GroupController>;
-  let mockMemberController: jest.Mocked<MemberController>;
 
   beforeEach(() => {
     mockGroupController = {
       createGroup: jest.fn(),
       deleteGroup: jest.fn(),
     } as any;
-
-    mockMemberController = {
-      addMemberToGroup: jest.fn(),
-      deleteMemberFromGroup: jest.fn(),
-    } as any;
   });
 
   it('should route POST /group to the createGroup method of GroupController', async () => {
     // Arrange
-    apiServer = new ApiServer(mockGroupController, mockMemberController);
+    apiServer = new ApiServer(mockGroupController);
     mockGroupController.createGroup.mockImplementation(async (req, res) => {
       res.status(201).send({ message: 'Group created' });
     });
